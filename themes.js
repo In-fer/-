22090f4f8f69265780823381f6e1db9eb6a2c672
In-fer <90282790+In-fer@users.noreply.github.com>
2021-09-24 10:08:32 +0300

    Add files via upload

---
+++ themes.js
@@ -0,0 +1,28 @@
+const btn = document.getElementById("theme-button");
+const link = document.getElementById("theme-link");
+
+btn.addEventListener("click", ChangeTheme);
+
+function ChangeTheme() {
+    let mainTheme = "main.css";
+    let darkTheme = "dark.css";
+    let currTheme = link.getAttribute("href");
+    let theme;
+
+    if (currTheme === mainTheme) {
+        currTheme = darkTheme;
+        theme = "main";
+    } else {
+        currTheme = mainTheme;
+        theme = "main";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    Save(theme);
+}
+function Save(theme) {
+    let Request = new XMLHttpRequest();
+    Request.open("GET", "themes.php?theme=" + theme, true);
+    Request.send();
+}
