5c7fe4cd499d4124ff87f638953d0c5d2fd604e0
In-fer <90282790+In-fer@users.noreply.github.com>
2021-10-01 14:04:03 +0300

    Update themes.php

--- themes.php
+++ themes.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
 
-if (isset($_GET["theme"])) {
+if(isset($_GET["theme"]))
+{
     $theme = $_GET["theme"];
+    
+    SetCookie("имя куки","$theme",time()+86400 * 30 * 12);
 
-    if ($theme == "main" || $theme == "dark") {
+    if($theme == "light" || $theme == "dark")
+    {
         $_SESSION["theme"] = $theme;
     }
 }
